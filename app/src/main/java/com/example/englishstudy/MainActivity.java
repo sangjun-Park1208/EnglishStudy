cfefd0121147576f55b3208d762d7e986d849afd
yeopseung <kwak991021@gmail.com>
2021-11-14 13:29:33 +0900

    MainActivity.xml & ProgressBar Update

--- app/src/main/java/com/example/englishstudy/MainActivity.java
+++ app/src/main/java/com/example/englishstudy/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.englishstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 //test
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress(50);
     }
 }
