529210c406686ca56d86f095529cc82d88c40302
yeopseung <kwak991021@gmail.com>
2021-12-01 14:30:54 +0900

    Stage_Item 수정 (정답,오답)

--- app/src/main/java/com/example/englishstudy/global/Stage_Item.java
+++ app/src/main/java/com/example/englishstudy/global/Stage_Item.java
@@ -5,11 +5,15 @@ import androidx.cardview.widget.CardView;
 public class Stage_Item {//stage:번호, running:진행중인지,다했는지
     private String stage;
     private String running;
+    private int correct;
+    private int wrong;
 
 
-    public Stage_Item(String stage, String running) {
+    public Stage_Item(String stage, String running, int correct, int wrong) {
         this.stage = stage;
         this.running = running;
+        this.correct = correct;
+        this.wrong = wrong;
     }
 
     public String getStage() {
@@ -26,5 +30,21 @@ public class Stage_Item {//stage:번호, running:진행중인지,다했는지
 
     public void setRunning(String running) {
         this.running = running;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
+
+    public int getWrong() {
+        return wrong;
+    }
+
+    public void setWrong(int wrong) {
+        this.wrong = wrong;
     }
 }
