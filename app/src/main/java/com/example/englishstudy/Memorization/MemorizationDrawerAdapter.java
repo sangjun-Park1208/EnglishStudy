a54d8ea105d53d1dfbc6a648f725bd40ab5bfbd1
sangjun-Park1208 <77184523+sangjun-Park1208@users.noreply.github.com>
2021-12-06 19:16:01 +0900

    [feat] something wrong

---
+++ app/src/main/java/com/example/englishstudy/Memorization/MemorizationDrawerAdapter.java
@@ -0,0 +1,86 @@
+package com.example.englishstudy.Memorization;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.englishstudy.R;
+
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class MemorizationDrawerAdapter extends RecyclerView.Adapter<MemorizationDrawerAdapter.ViewHolder> implements MemorizationVocaItemClickListener{
+
+    private ArrayList<MemorizationVocaItem> mVocaList;
+    MemorizationVocaItemClickListener listener;
+
+    public MemorizationDrawerAdapter(ArrayList<MemorizationVocaItem> mVocaList, MemorizationVocaItemClickListener listener) {
+        this.mVocaList = mVocaList;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.memorization_drawer_item, parent, false);
+        return new ViewHolder(view, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        holder.onBind(mVocaList.get(position));
+    }
+
+    public void setOnItemClicklistener(MemorizationVocaItemClickListener listener){
+        this.listener=listener;
+    }
+
+    @Override
+    public void onItemClick(ViewHolder holder, View view, int position) {
+        if(listener != null){
+            listener.onItemClick(holder, view, position);
+        }
+    }
+
+
+    public void setmVocaList(ArrayList<MemorizationVocaItem> list){
+        this.mVocaList=list;
+        notifyDataSetChanged();
+    }
+    @Override
+    public int getItemCount() {
+        return mVocaList.size();
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView voca;
+        public ViewHolder(@NonNull @NotNull View itemView, final MemorizationVocaItemClickListener listener) {
+            super(itemView);
+            voca = itemView.findViewById(R.id.mem_drawer_item);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position=getAdapterPosition();
+                    if(listener!=null){
+                        listener.onItemClick(ViewHolder.this,v,position);
+                    }
+                }
+            });
+        }
+
+        public void onBind(MemorizationVocaItem vocaItem){
+            voca.setText(vocaItem.getVoca());
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/englishstudy/Memorization/MemorizationVocaItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.englishstudy.Memorization;
+
+import android.view.View;
+
+public interface MemorizationVocaItemClickListener {
+    void onItemClick(MemorizationDrawerAdapter.ViewHolder holder, View view, int position);
+}
