4ae1a01fd32b285603ce90aa664fc7b5462bda67
yeopseung <kwak991021@gmail.com>
2021-12-08 01:34:01 +0900

    TEST & Main 로그 추가

--- app/src/main/java/com/example/englishstudy/Test/TestResult.java
+++ app/src/main/java/com/example/englishstudy/Test/TestResult.java
@@ -1,8 +1,5 @@
 package com.example.englishstudy.Test;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,17 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.englishstudy.R;
 
 public class TestResult extends AppCompatActivity {
-    private Intent intent;
-
-    private TextView test_tv_Stage;
-    private TextView test_tv_Correct;
-    private TextView test_tv_Wrong;
-
-    private Button test_bt_Retry;
-    private Button test_bt_OK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +29,30 @@ public class TestResult extends AppCompatActivity {
         int wrong;
 
         //Intent 로 stage, 정답 개수, 오답 개수 전달 받음
-        intent = getIntent();
+        Intent intent = getIntent();
         stage = intent.getIntExtra("stage", 0);
         correct = intent.getIntExtra("correct", 0);
         wrong = intent.getIntExtra("wrong", 0);
 
+        Log.d("TEST","Stage:"+(stage+1));
+        Log.d("TEST","정답:"+correct);
+        Log.d("TEST","오답:"+wrong);
+        Log.d("TEST","결과창에 전달 완료");
+
         //TextView, Button 참조
-        test_tv_Stage = findViewById(R.id.test_tv_Stage);
+        TextView test_tv_Stage = findViewById(R.id.test_tv_Stage);
         test_tv_Stage.setText("Stage " + Integer.toString(stage+1));
 
-        test_tv_Correct = findViewById(R.id.test_tv_Correct);
+        TextView test_tv_Correct = findViewById(R.id.test_tv_Correct);
         test_tv_Correct.setText("정답: " + Integer.toString(correct));
 
-        test_tv_Wrong = findViewById(R.id.test_tv_Wrong);
+        TextView test_tv_Wrong = findViewById(R.id.test_tv_Wrong);
         test_tv_Wrong.setText("오답: " + Integer.toString(wrong));
 
-        test_bt_OK = findViewById(R.id.test_bt_ok);
-        test_bt_Retry = findViewById(R.id.test_bt_Retry);
+        Button test_bt_OK = findViewById(R.id.test_bt_ok);
+        Button test_bt_Retry = findViewById(R.id.test_bt_Retry);
 
-        //OK 버튼 클릭 시
+        //DONE 버튼 클릭 시
         test_bt_OK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,6 +61,13 @@ public class TestResult extends AppCompatActivity {
                 intent.putExtra("stage",stage);
                 intent.putExtra("correct", correct);
                 intent.putExtra("wrong", wrong);
+
+                Log.d("TEST","DONE 클릭");
+                Log.d("TEST","Stage:"+(stage+1));
+                Log.d("TEST","정답:"+correct);
+                Log.d("TEST","오답:"+wrong);
+                Log.d("TEST","TestList 에 전달");
+
                 startActivity(intent);
                 finish();
             }
@@ -76,6 +80,10 @@ public class TestResult extends AppCompatActivity {
                 //해당 Stage Test 다시 시작
                 Intent intent = new Intent(v.getContext(), TestActivity.class);
                 intent.putExtra("Stage",stage);
+
+                Log.d("TEST","Retry 클릭");
+                Log.d("TEST","Stage:"+(stage+1)+"재시작");
+
                 startActivity(intent);
                 finish();
             }
